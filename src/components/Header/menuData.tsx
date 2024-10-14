cfeb52f773e6adb6e7264d5bb37f3142257664c4
gzyxds <348310633@qq.com>
2024-10-14 22:11:37 +0800

    172号卡官网

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -40,7 +40,7 @@ const menuData: Menu[] = [
   },
   {
     id: 4,
-    title: "页面",
+    title: "其他",
     newTab: false,
     // 定义子菜单
     submenu: [
@@ -52,45 +52,63 @@ const menuData: Menu[] = [
       },
       {
         id: 42,
-        title: "联系页面",
+        title: "联系我们",
         path: "/contact",
         newTab: false,
       },
-      {
-        id: 43,
-        title: "博客网格页面",
-        path: "/blog",
-        newTab: false,
-      },
-      {
-        id: 44,
-        title: "博客边栏页面",
-        path: "/blog-sidebar",
-        newTab: false,
-      },
-      {
-        id: 45,
-        title: "博客详情页面",
-        path: "/blog-details",
-        newTab: false,
-      },
+      // {
+      //   id: 43,
+      //   title: "博客网格页面",
+      //   path: "/blog",
+      //   newTab: false,
+      // },
+      // {
+      //   id: 44,
+      //   title: "博客边栏页面",
+      //   path: "/blog-sidebar",
+      //   newTab: false,
+      // },
+      // {
+      //   id: 45,
+      //   title: "博客详情页面",
+      //   path: "/blog-details",
+      //   newTab: false,
+      // },
       {
         id: 46,
-        title: "登录页面",
+        title: "代理登入",
         path: "https://haoka.lot-ml.com/plugreg.html?agentid=90925",
         newTab: false,
       },
+      // {
+      //   id: 47,
+      //   title: "注册页面",
+      //   path: "https://haoka.lot-ml.com/plugreg.html?agentid=90925", 
+      //   path: "/signup", // 注册页面路径
+      //   newTab: false,
+      // },
       {
-        id: 47,
-        title: "注册页面",
-        path: "https://haoka.lot-ml.com/plugreg.html?agentid=90925", 
-        path: "/signup", // 注册页面路径
+        id: 48,
+        title: "官方客服",
+        path: "https://syx0xk.qiyukf.com/client?k=1bba63d184c3fecce3532aacae755c47&wp=1&robotShuntSwitch=0",
         newTab: false,
       },
       {
-        id: 48,
-        title: "错误页面",
-        path: "/error",
+        id: 49,
+        title: "一证通查",
+        path: "https://getsimnum.caict.ac.cn/",
+        newTab: false,
+      },
+      {
+        id: 50,
+        title: "订单查询",
+        path: "https://haokawx.lot-ml.com/Search/Index",
+        newTab: false,
+      },
+      {
+        id: 51,
+        title: "加入群聊",
+        path: "https://work.weixin.qq.com/wework_admin/external_room/jump_from_doc?code=roomCode_mbUiBp1sFWjy7VvS&bar_style_type=5",
         newTab: false,
       },
     ],
