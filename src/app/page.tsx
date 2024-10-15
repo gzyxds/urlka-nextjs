0383e7c941871d8d1f71495b5edf8a2c24092d5e
gzyxds <348310633@qq.com>
2024-10-15 19:34:22 +0800

    172号卡官网

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,18 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "172号卡分销系统官网",
-  description: "172号卡分销系统官网",
-  // other metadata
+  title: "172号卡分销系统官网_手机流量卡分销管理平台",  // 网站标题
+  keywords: "172号卡官网,手机流量卡,流量卡,流量卡分销,流量卡批发,流量卡代理,流量卡分销系统,流量卡分销平台,流量卡分销管理,流量卡分销系统官网",  // 网站关键词
+  description: "172号卡官网！我们提供最新的电信、移动、联通和广电流量卡及物联卡的在线申请和办理服务。加入我们的分销管理平台，成为代理合伙人，轻松管理您的流量卡业务，获取丰厚收益。",  // 网站描述
+  openGraph: {
+    images: ["/images/og-image.png"],  // 网站图片
+  },
 };
 
 export default function Home() {
   return (
     <>
-      <ScrollUp />
+      <ScrollUp /> 
       <Hero />
       <Features />
       <Video />
