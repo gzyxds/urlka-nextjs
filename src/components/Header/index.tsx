cf5eecb0d8eb8c0525de44d056d8b63d1801111c
gzyxds <348310633@qq.com>
2024-10-21 17:03:50 +0800

    更新 index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import ThemeToggler from "./ThemeToggler";
+import ThemeToggler from "./ThemeToggler";  
 import menuData from "./menuData";
 
 const Header = () => {
@@ -196,7 +196,8 @@ const Header = () => {
                  登入
                 </a>
                 <div>
-                 { <ThemeToggler onToggle={(isDark) => setIsDark(isDark)} /> }
+                  {/* 主题切换器 */}
+                  <ThemeToggler />
                 </div>
               </div>
             </div>
